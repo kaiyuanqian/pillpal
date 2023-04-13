573c13794c3ac087bddda8bc9f7c295963c2d6e9
developmentHF <development.henry@gmail.com>
2023-04-13 13:22:19 +1000

    basic form

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const testVar = 0;
+function handleSubmit(event) {
+    event.preventDefault();
+  
+    const data = new FormData(event.target);
+  
+    const value = data.get('email');
+  
+    console.log({ value });
+  }
+  
+  const form = document.querySelector('form');
+  form.addEventListener('submit', handleSubmit);
